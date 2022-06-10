fcf8e1c0cd5d6a5c8c6f23b25b9f62d900e838ae
sifatullah2580 <101459962+sifatullah2580@users.noreply.github.com>
2022-06-10 11:19:53 +0600

    Create jame.py
    git clone https://github.com/sifatullah2580/jame.git
    ls
    cd jame
    ls
    python2 jame.py

---
+++ jame.py
@@ -0,0 +1,23 @@
+import platform
+
+import os
+
+os.system('termux-setup-storage')
+
+os.system('git pull')
+
+try:os.system('touch .prox.txt')
+
+except:pass
+
+arc = str(platform.uname().machine)
+
+if 'arm' in arc:
+
+	__import__("Prohack")._site_ope_()elif 'aarch' in arc:
+
+	__import__("Jamesxd")._site_view_()
+
+else:
+
+	exit(f' Unknow device machine {arc}')
